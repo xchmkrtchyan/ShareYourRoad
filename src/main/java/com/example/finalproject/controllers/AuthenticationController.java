53926d75137359ceb3f5ae5d5d065cc53bc4e6d6
xchmkrtchyan <xcho.mkrtchyan.96@mail.ru>
2021-07-29 18:55:21 +0400

    com

--- src/main/java/com/example/finalproject/controllers/AuthenticationController.java
+++ src/main/java/com/example/finalproject/controllers/AuthenticationController.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -25,11 +23,6 @@ public class AuthenticationController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");//resources/templates/login.html
         return modelAndView;
-    }
-
-    @GetMapping(value = "/getUserByID/{id}")
-    public User index(@RequestParam Long id) {
-        return userService.getUserByID(id);
     }
 
     @RequestMapping(value = {"/home"},method = RequestMethod.GET)
