429dd725b99df8e9edb52cb637efc90bfa13a16e
xchmkrtchyan <xcho.mkrtchyan.96@mail.ru>
2021-08-01 21:07:19 +0400

    global update

--- src/main/java/com/example/finalproject/models/Role.java
+++ src/main/java/com/example/finalproject/models/Role.java
@@ -1,67 +1,39 @@
 package com.example.finalproject.models;
 
-
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
-@Table(name = "auth_role")
+@Table(name = "roles")
 public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "auth_role_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Column(name = "role_name")
-    private String role;
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
 
-    @Column(name = "role_desc")
-    private String desc;
+	public Role() {
 
-    @ManyToMany(mappedBy = "roles")
-    private List<User> users;
+	}
 
+	public Role(ERole name) {
+		this.name = name;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getRole() {
-        return role;
-    }
+	public ERole getName() {
+		return name;
+	}
 
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
-    @Override
-    public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", role='" + role + '\'' +
-                ", desc='" + desc + '\'' +
-                ", users=" + users +
-                '}';
-    }
+	public void setName(ERole name) {
+		this.name = name;
+	}
 }
-
--- src/main/java/com/example/finalproject/repository/RoleRepository.java
+++ src/main/java/com/example/finalproject/repository/RoleRepository.java
@@ -1,11 +1,13 @@
 package com.example.finalproject.repository;
 
+import com.example.finalproject.models.ERole;
 import com.example.finalproject.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Long> {
+import java.util.Optional;
 
-    Role findByDesc(String role);
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(ERole name);
 }
