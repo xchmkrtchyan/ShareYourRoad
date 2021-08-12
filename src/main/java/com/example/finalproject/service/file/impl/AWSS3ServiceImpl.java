e14d11b99d91a64cdf04a5f74962414b86e2cb98
xchmkrtchyan <xcho.mkrtchyan.96@mail.ru>
2021-08-12 15:29:54 +0400

    added pom dependency

--- src/main/java/com/example/finalproject/service/file/impl/AWSS3ServiceImpl.java
+++ src/main/java/com/example/finalproject/service/file/impl/AWSS3ServiceImpl.java
@@ -16,7 +16,6 @@ import com.example.finalproject.service.userDetails.model.MyUserDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
