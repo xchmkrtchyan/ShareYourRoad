a64035a699032af2c0ccfcf6707f5b3a90c27cab
xchmkrtchyan <xcho.mkrtchyan.96@mail.ru>
2021-08-12 13:17:20 +0400

    add comment

--- src/main/java/com/example/finalproject/service/user/UserService.java
+++ src/main/java/com/example/finalproject/service/user/UserService.java
@@ -4,6 +4,7 @@ package com.example.finalproject.service.user;
 import com.example.finalproject.rest.model.UserRequest;
 
 public interface UserService {
+    //
     Boolean existsByUsername(UserRequest userRequest);
 
     Boolean existsByEmail(UserRequest userRequest);
